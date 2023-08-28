262b7b20ea39900672fd4baf543074b137a11636
Raj Kadiyala <rskadiy@gmail.com>
2023-08-28 02:53:10 +0000

    Add basic document indexing capability

--- index.py
+++ index.py
@@ -2,8 +2,11 @@
 
 import re
 import string
+import typing
 
 import nltk
+
+Index: typing.TypeAlias = dict[str, list[int]]
 
 _PUNCTUATION = re.compile(f'[{re.escape(string.punctuation)}]')
 _STEMMER = nltk.stem.PorterStemmer()
@@ -15,7 +18,7 @@ _STOP_WORDS = set(['the', 'be', 'to', 'of', 'and', 'a', 'in', 'that', 'have',
 class _Tokenizer:
 
     def tokenize(self, text: str) -> list[str]:
-        '''Converts text into distinct words for indexing'''
+        """Converts text into distinct words for indexing."""
         tokens = self._tokenize(text)
         tokens = self._lowercase(tokens)
         tokens = self._remove_punctuation(tokens)
@@ -37,3 +40,21 @@ class _Tokenizer:
 
     def _remove_common_words(self, tokens: list[str]) -> list[str]:
         return [token for token in tokens if token not in _STOP_WORDS]
+
+
+class DocumentIndexer:
+    """Utility class for indexing documents."""
+
+    def index_by_page_number(self, document: list[str]) -> Index:
+        """Creates a page number index for various words within a document."""
+        page_number_index: Index = {}
+        for page_number, page in enumerate(document):
+            self._index_page(page, page_number, page_number_index)
+        return page_number_index
+
+    def _index_page(self, page: str, page_number: int, page_number_index: Index) -> None:
+        for token in _Tokenizer().tokenize(page):
+            if token in page_number_index:
+                page_number_index[token].append(page_number)
+            else:
+                page_number_index[token] = [page_number]
